41a2e7365c0821fe596f7c377b0162955725df8e
kuldeep3 <kuldeepupreti3@gmail.com>
2020-07-17 15:30:11 +0530

    string manipulation

--- elixirtut.ex
+++ elixirtut.ex
@@ -1,13 +1,25 @@
-defmodule Elixirtut do
-    def main do
-        do_stuff()
-    end
+defmodule M do
+  def main do
+    do_stuff()
+  end
 
-    def do_stuff do
-        my_str = "My Sentence"
-        IO.puts "Length : #{String.length(my_str)}"
-        longer_str = my_str <> " " <> "is longer"
-        IO.puts "Equal: #{"Egg" === "egg"}"
-    end
+  def do_stuff do
+    my_str = "My Sentence"
+    IO.puts("Length : #{String.length(my_str)}")
+    longer_str = my_str <> " " <> "is longer"
+    IO.puts("My ? #{String.contains?(my_str, "My")}")
 
+    IO.puts("First: #{String.first(my_str)}")
+    IO.puts("Index 4: #{String.at(my_str, 4)}")
+    IO.puts("Sustring: #{String.slice(my_str, 3, 8)}")
+
+    IO.inspect(String.split(longer_str, " "))
+
+    IO.puts(String.reverse(longer_str))
+    IO.puts(String.upcase(longer_str))
+    IO.puts(String.downcase(longer_str))
+    IO.puts(String.capitalize(longer_str))
+
+    (4 * 10) |> IO.puts()
+  end
 end
