1568f6425d91adc32a11ad1f19ae79bc1ce06ced
kuldeep3 <kuldeepupreti3@gmail.com>
2020-07-17 17:22:03 +0530

    recent changes

---
+++ Lists.ex
@@ -0,0 +1,42 @@
+defmodule Decision do
+  def main do
+    do_stuff()
+  end
+
+  def do_stuff do
+    list1 = [1, 2, 3]
+    list2 = [4, 5, 6]
+
+    list3 = list1 ++ list2
+    list4 = list3 -- list1
+    IO.puts(6 in list4)
+
+    [head | tail] = list3
+    IO.puts("Head : #{head}")
+    IO.write("Tail : ")
+    IO.inspect(tail)
+    IO.inspect([97, 98], charlists: :as_lists)
+
+    Enum.each(tail, fn item ->
+      IO.puts(item)
+    end)
+
+    words = ["Random", "Words", "in a", "list"]
+
+    Enum.each(words, fn word ->
+      IO.puts(word)
+    end)
+
+    display_list(words)
+
+    IO.puts(display_list(List.delete_at(words, 1)))
+    IO.puts(display_list(List.insert_at(words, 4, "Yeah")))
+  end
+
+  def display_list([word | words]) do
+    IO.puts(word)
+    display_list(words)
+  end
+
+  def display_list([]), do: nil
+end
