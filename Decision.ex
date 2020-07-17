112b08afa7da741739c008622425fca71e72b95a
kuldeep3 <kuldeepupreti3@gmail.com>
2020-07-17 16:33:53 +0530

    recent changes

--- Decision.ex
+++ Decision.ex
@@ -32,5 +32,18 @@ defmodule Decision do
     end
 
     IO.puts("Ternary: #{if age > 18, do: "Can Vote", else: "Can't Vote"}")
+
+    my_stats = {175, 6.25, :Derek}
+    IO.puts("Tuple #{is_tuple(my_stats)}")
+    my_stats2 = Tuple.append(my_stats, 42)
+    IO.puts("Age #{elem(my_stats2, 3)}")
+    IO.puts("Size : #{tuple_size(my_stats2)}")
+
+    my_stats3 = Tuple.delete_at(my_stats2, 0)
+    my_stats4 = Tuple.insert_at(my_stats3, 0, 1974)
+
+    many_zeroes = Tuple.duplicate(0, 5)
+    {weight, height, name} = {175, 6.25, "Derek"}
+    IO.puts("Weight : #{weight}")
   end
 end
