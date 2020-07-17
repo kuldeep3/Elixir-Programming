897d2eb1f36d1d3beb2554643f5744d4e1672fdb
kuldeep3 <kuldeepupreti3@gmail.com>
2020-07-17 16:07:34 +0530

    logical operation

--- Maths.ex
+++ Maths.ex
@@ -14,5 +14,8 @@ defmodule Maths do
     IO.puts("4 === 4.0: #{4 === 4.0}")
     IO.puts("4 != 4.0: #{4 != 4.0}")
     IO.puts("4 !== 4.0: #{4 !== 4.0}")
+    age = 16
+    IO.puts("Vote & Drive : #{age >= 16 and age >= 18}")
+    IO.puts("Vote or Drive : #{age >= 16 or age >= 18}")
   end
 end
